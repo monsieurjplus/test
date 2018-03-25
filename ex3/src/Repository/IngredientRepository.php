d80c91282ed2d86eabd19c34b9c5f8c7deb8c1a3
monsieurjplus <jourdan.buatois@gmail.com>
2018-03-26 00:35:34 +0200

    ex3 - added test ingredients DB file

---
+++ ex3/src/Repository/IngredientRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+// src/Repository/IngredientRepository.php
+namespace App\Repository;
+
+use App\Entity\Ingredient;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class IngredientRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Ingredient::class);
+    }
+
+    public function findAllNameOfGroupNameMatch($word) : array
+    {
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager
+            ->createQuery(
+                'SELECT i.code AS id, i.name AS name
+                FROM App\Entity\Ingredient i
+                WHERE (i.name LIKE :word)
+                ORDER BY i.name ASC'
+            )
+            ->setParameter('word', '%' . $word . '%');
+
+        return $query->getResult();
+    }
+}
